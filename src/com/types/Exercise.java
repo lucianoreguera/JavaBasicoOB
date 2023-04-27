66520a71a53634846216baa28de2458585868ab7
lucianor <regueraluciano@gmail.com>
2023-04-27 08:07:45 -0300

    Exercise 1 - Session 1

---
+++ src/com/types/Exercise.java
@@ -0,0 +1,27 @@
+package com.types;
+
+public class Exercise {
+    public static void main(String[] args) {
+        byte option = 5;
+        System.out.println("Opción ingresada" + option);
+        short age = 42;
+        System.out.println("Edad: " + age);
+        int yearOfBirth = 1980;
+        System.out.println("Año de nacimiento: " + yearOfBirth);
+        long dni = 28123456;
+        System.out.println("DNI: " + dni);
+
+        float pi = 3.14f;
+        System.out.println("PI: " + pi);
+        double average = 123.14456;
+        System.out.println("Promedio" + average);
+
+        char character = 'A';
+        System.out.println("Caracter: " + character);
+        String fullName = "Max Power";
+        System.out.println("Nombre completo: " + fullName);
+
+        boolean isAdmin = true;
+        System.out.println("Es administrador: " + isAdmin);
+    }
+}
