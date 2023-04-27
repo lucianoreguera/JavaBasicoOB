a232f9ede9d0f5e36f89b908446bbe9fdcb9f15e
lucianor <regueraluciano@gmail.com>
2023-04-27 08:17:00 -0300

    Session 2

---
+++ src/com/functions/Overload.java
@@ -0,0 +1,20 @@
+// Sobrecarga de Funciones
+package com.functions;
+
+public class Overload {
+    public static void main(String[] args) {
+
+    }
+
+    static double suma(double numero1, double numero2){
+        return numero1 + numero2;
+    }
+
+    static int suma(int numero1, int numero2){
+        return numero1 + numero2;
+    }
+
+    static int suma(int numero1, int numero2, int numero3){
+        return numero1 + numero2 + numero3;
+    }
+}
