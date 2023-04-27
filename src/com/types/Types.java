a43809685c435986d821cfd04f1216267899e1b0
lucianor <regueraluciano@gmail.com>
2023-04-27 07:46:19 -0300

    First Commit

---
+++ src/com/types/Types.java
@@ -0,0 +1,25 @@
+package com.types;
+
+public class Types {
+    public static void main(String[] args) {
+        // 1. Enteros
+        byte var1 = 5;
+        short var2 = 14;
+        int var3 = 123;
+        long var4 = 1234;
+
+        // 2. Flotantes
+        float var5 = 5.5f;
+        double var6 = 12.4; // o 12.4d
+
+        // 3. Booleanos
+        boolean var7 = false;
+        boolean var8 = true;
+
+        // 4. Cadenas de Texto
+        char var9 = 'a';
+        String var10 = "Hola Mundo";
+
+        // Los tipos primitivos no aceptan nulos
+    }
+}
